4fdf1a74949002ef080980370d7410087345feec
niko <niko@niko.io>
2021-06-15 16:20:33 +0200

    spacing adjustment

--- src/digger-modules/activities/bubble-mix-3.tsx
+++ src/digger-modules/activities/bubble-mix-3.tsx
@@ -6,11 +6,11 @@ const data = {
     title: "Bubble Mix",
     description: "How many bubbles can you count?",
     tips: "Technical tip on video : Frame-by-Frame with VLC and slow down the speed!",
-    solution: 99,
-    options: [95, 96, 97, 98, 99, 100,]},
+    solution: 26,
+    options: [23, 24, 25, 26, 27, 28]},
   media: {
     type: "video" as const,
-    src: "https://digger-training-modules-resources.s3.eu-central-1.amazonaws.com/resources/bubbles/bubble-3.mp4"
+    src: "https://digger-training-modules-resources.s3.eu-central-1.amazonaws.com/resources/bubbles/bubbles_advanced_new.mp4"
   }
 }
 
