e38f773775d04689071c01a28f5ba7cbd5beccb2
niko <niko@niko.io>
2021-06-17 15:51:21 +0200

    added the new gas station example to everything

--- src/digger-modules/activities/20-hints-3-gas-station.tsx
+++ src/digger-modules/activities/20-hints-3-gas-station.tsx
@@ -16,110 +16,112 @@ const data: types.Data = {
     description: "Extract 20 pieces of information hinting to the specific location"
   },
   image: {
-    src: "https://digger-training-modules-resources.s3.eu-central-1.amazonaws.com/resources/20-hints/20-hints-3_f.jpg",
+    src: "https://digger-training-modules-resources.s3.eu-central-1.amazonaws.com/resources/20-hints/20-hints-4.jpg",
     height: 1365,
     width: 1024,
   },
   shapes: [
     { description: "The whole thing",
       shape:
-        <path d="M5.67737 529.107V5.79224H1007.9V556.263L5.67737 529.107Z"
-              transform="translate(3,219)"/>
+        <path d="M2 1039.18V2H1022V1093L2 1039.18Z"
+              transform="translate(4,15)" />
     },
-    { description: "Graffitti",
+    { description: "somegraffiti in the back",
       shape:
-        <path d="M5 23.6749V124.453L425.632 134.066V6L5 18.4034"
+        <path d="M2 19.6749V120.453L422.632 130.066V2L2 14.4034"
               transform="translate(531,550)"/>
     },
-    { description: "Trees, time of year",
+    { description: "monstera graffiti in the back",
       shape:
-        <path d="M6 163.663V8L115.755 29.0859L119.291 96.3746L122.827 163.663H6Z"
-              transform="translate(9,398)"/>
+        <path d="M2 238.5L8.5 2H66.5L48.5 238.5H2Z"
+              transform="translate(953,476)" />
     },
-    { description: "fence",
-      shape:
-        <path d="M6 101.576V7L402.871 18.7833V84.8316L6 101.576Z"
-              transform="translate(21,578)"/>
-    },
-    { description: "van",
-      shape:
-        <path d="M11.2155 102.29L6.40662 6.78296L189.708 18.2562L252.506 108.491L11.2155 102.29Z"
-              transform="translate(16, 549)"/>
-    },
-    { description: "electric box 1",
-      shape:
-        <path d="M6 82V6H75V82H6Z"
-              transform="translate(137,448)"/>
-    },
-    { description: "metal staircase 1",
-      shape:
-        <path d="M6 103.367V6H31.7414V103.367H6Z"
-              transform="translate(201,455)"/>
-    },
-    { description: "sign 1",
-      shape:
-        <path d="M10.4194 47L3 7.675L26 3V47H10.4194Z"
-              transform="translate(252,531)"/>
-    },
-    { description: "sign 2",
-      shape:
-        <path d="M48.9569 18.8143H2V5.17059L48.9569 3V18.8143Z"
-              transform="translate(232,575)"/>
-    },
-    { description: "boxy green building",
-      shape:
-        <path d="M80.8028 132.964L11.2254 145L7 8L87 25.7003L80.8028 132.964Z"
-              transform="translate(314,460)"/>
-    },
-    { description: "sign 3",
-      shape:
-        <path d="M2 22.1556V2H22.9326V22.1556H2Z"
-              transform="translate(327,468)"/>
-    },
-    { description: "sign 4",
-      shape:
-        <path d="M2 26.8766V11.0622L27.1757 3V26.8766H2Z"
-              transform="translate(329,563)"/>
-    },
-    { description: "sign 5",
-      shape:
-        <path d="M2 18.7447V2H28.5901V18.7447H2Z"
-              transform="translate(357,564)"/>
-    },
-    { description: "sign 6",
-      shape:
-        <path d="M2 14V2H17V14H2Z"
-              transform="translate(396,583)"/>
-    },
-    { description: "poller",
-      shape:
-        <path d="M6.69946 58.236V13.2736V6.1416H20.5602V13.2736V58.236H6.69946Z"
-              transform="translate(399,611)" />
-    },
-    { description: "electric box 2",
-      shape:
-        <path d="M8 138.518V10L72 21.5666L68.6316 147L8 138.518Z"
-              transform="translate(724,333)"/>
-    },
-    { description: "trash can",
-      shape:
-        <path d="M48.9967 56.234V6H6V56.234H48.9967Z"
-              transform="translate(910,421)"/>
-    },
-    { description: "another random black box",
+    { description: "a mysterious black box",
       shape:
         <path d="M4.23077 5.8V59H64V5.8L1 2"
               transform="translate(718,514)"/>
     },
-    { description: "a cute unicorn",
+    { description: "a very pretty unicorn",
       shape:
         <path d="M2.31372 85.6119V2.19873H51.5336L58.6055 80.3404L2.31372 85.6119Z"
               transform="translate(708,713)"/>
     },
-    { description: "bottle cap",
+    { description: "a coca cola bottlecap",
       shape:
-        <path d="M16.3746 30.7759L11 13.1009L31.084 10L32.7812 30.7759H16.3746Z"
-              transform="translate(570,1206)"/>
+        <path d="M7.37458 22.7759L2 5.10088L22.084 2L23.7812 22.7759H7.37458Z"
+              transform="translate(580,1216)"/>
+    },
+    { description: "a blue van",
+      shape:
+        <path d="M1.5 27V2.5L175 8.5L183.5 20.5L239.5 75V98.5L171.5 111.5L175 35.5L1.5 27Z"
+              transform="translate(25.5,562)"/>
+    },
+    { description: "fence",
+      shape:
+        <path d="M2 94V2L127 19V78L2 94Z"
+              transform="translate(1,584)"/>
+    },
+    { description: "trees in the summer",
+      shape:
+        <path d="M2 157.663V2L111.755 23.0859L115.291 90.3746L118.827 157.663H2Z"
+              transform="translate(9,398)"/>
+    },
+    { description: "cool spiral staircase",
+      shape:
+        <path d="M2 99.3671V2H27.7414V99.3671H2Z"
+              transform="translate(201,455)"/>
+    },
+    { description: "a traffic sign",
+      shape:
+        <path d="M9.41935 46L2 6.675L25 2V46H9.41935Z"
+              transform="translate(252,531)"/>
+    },
+    { description: "yellow berlin bus",
+      shape:
+        <path d="M118 81V11.5L4 2.5L61 63L118 81Z"
+              transform="translate(205,570.5)"/>
+    },
+    { description: "the sign of the bus",
+      shape:
+        <path d="M48.9569 17.8143H2V4.17059L48.9569 2V17.8143Z"
+              transform="translate(232,575)"/>
+    },
+    { description: "a green building",
+      shape:
+        <path d="M75.8028 126.964L6.22535 139L2 2L82 19.7003L75.8028 126.964Z"
+              transform="translate(314,460)"/>
+    },
+    { description: "a power box on the green building",
+      shape:
+        <path d="M2 22.1556V2H22.9326V22.1556H2Z"
+              transform="translate(327,468)"/>
+    },
+    { description: "a red flag",
+      shape:
+        <path d="M2 26.8766V11.0622L27.1757 3V26.8766H2Z"
+              transform="translate(329,563)"/>
+    },
+    { description: "a yellow sign",
+      shape:
+        <path d="M2 18.7447V2H28.5901V18.7447H2Z"
+              transform="translate(357,564)"/>
+    },
+    { description: "a poller",
+      shape:
+        <path d="M1.69946 54.236V9.27358V2.1416H15.5602V9.27358V54.236H1.69946Z"
+              transform="translate(399,611)"/>
+    },
+    { description: "a dude sitting, a person",
+      shape:
+        <path d="M1.5 39.5V2H26V39.5H1.5Z"
+              transform="translate(281.5,622)"/>
+    },
+    { description: "mysterious power boxes",
+      shape:
+        <g transform="translate(139,333)">
+          <path d="M2 183V129H64V183H2Z" />
+          <path d="M587 130.518V2L651 13.5666L647.632 139L587 130.518Z" />
+        </g>
     },
   ]
 }
