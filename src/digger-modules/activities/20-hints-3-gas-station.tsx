4726114aaaccd1972c7616cf39ace2cd8f581c91
niko <niko@niko.io>
2021-06-21 10:27:52 +0200

    oops

--- src/digger-modules/activities/20-hints-3-gas-station.tsx
+++ src/digger-modules/activities/20-hints-3-gas-station.tsx
@@ -29,10 +29,10 @@ const data: types.Data = {
     { description: "gas station bottoms",
       shape:
         <g transform="translate(36, 664)">
-          <path d="M780.5 189L413.5 115L471.5 70L803 137.5L780.5 189Z" stroke="#F70F39" stroke-width="3"/>
-          <path d="M203 58.5L117.5 70L5 47.5L63 12L203 58.5Z" stroke="#F70F39" stroke-width="3"/>
-          <path d="M527.5 36L379.5 21.5L404 2.5L571 21.5L527.5 36Z" stroke="#F70F39" stroke-width="3"/>
-          <path d="M981.5 70L753 47.5L780.5 36L989.5 54L981.5 70Z" stroke="#F70F39" stroke-width="3"/>
+          <path d="M780.5 189L413.5 115L471.5 70L803 137.5L780.5 189Z" />
+          <path d="M203 58.5L117.5 70L5 47.5L63 12L203 58.5Z"/>
+          <path d="M527.5 36L379.5 21.5L404 2.5L571 21.5L527.5 36Z" />
+          <path d="M981.5 70L753 47.5L780.5 36L989.5 54L981.5 70Z" />
         </g>
     },
     { description: "somegraffiti in the back",
